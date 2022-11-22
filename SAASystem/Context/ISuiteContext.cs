32fbb9f0ac313d5917334bf1522f56583ed850df
Gayan Kuruppu <gayan.kuruppu@live.com>
2022-11-22 10:25:50 +0000

    feat(core): create suite context

---
+++ SAASystem/Context/ISuiteContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SAASystem.Context
+{
+    public interface ISuiteContext
+    {
+        SuiteModel Select(int suiteId);
+        IEnumerable<SuiteModel> SelectAll();
+        int Insert(string name, double cpw, double size, double securityDeposit,
+            int daysAvailable, DateTime contractFrom, DateTime conractTo,
+            int maximumStay, int isShortTermLet, int isAmneties, int isFurnish,
+            int isReference, int isBills, int isBroadband);
+        int Delete(int suiteId);
+    }
+}
