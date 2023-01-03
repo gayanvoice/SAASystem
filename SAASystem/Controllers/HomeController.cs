4442af92cb6906fa41814bbf6707cda17a1bc960
Gayan Kuruppu <gayan.kuruppu@live.com>
2023-01-03 15:06:46 +0000

    Add Pics

--- SAASystem/Controllers/HomeController.cs
+++ SAASystem/Controllers/HomeController.cs
@@ -20,61 +20,61 @@ namespace SAASystem.Controllers
             {
                 Name = "Apartment",
                 Route = new ItemComponentModel.RouteModel() { Controller = "Apartment", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/apartment.jpg"
             });
             itemModelList.Add(new ItemComponentModel()
             {
                 Name = "Contract",
                 Route = new ItemComponentModel.RouteModel() { Controller = "Contract", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/contract.jpg"
             });
             itemModelList.Add(new ItemComponentModel()
             {
                 Name = "Employee",
                 Route = new ItemComponentModel.RouteModel() { Controller = "Employee", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/employee.jpg"
             });
             itemModelList.Add(new ItemComponentModel()
             {
                 Name = "Property",
                 Route = new ItemComponentModel.RouteModel() { Controller = "Property", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/property.jpg"
             });
             itemModelList.Add(new ItemComponentModel()
             {
                 Name = "Role",
                 Route = new ItemComponentModel.RouteModel() { Controller = "Role", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/role.jpg"
             });
             itemModelList.Add(new ItemComponentModel()
             {
                 Name = "Room",
                 Route = new ItemComponentModel.RouteModel() { Controller = "Room", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/room.jpg"
             });
             itemModelList.Add(new ItemComponentModel()
             {
                 Name = "Stock",
                 Route = new ItemComponentModel.RouteModel() { Controller = "Stock", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/stock.jpg"
             });
             itemModelList.Add(new ItemComponentModel()
             {
                 Name = "Suite",
                 Route = new ItemComponentModel.RouteModel() { Controller = "Suite", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/suite.jpg"
             });
             itemModelList.Add(new ItemComponentModel()
             {
                 Name = "Tenant",
                 Route = new ItemComponentModel.RouteModel() { Controller = "Tenant", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/tenant.jpg"
             });
             itemModelList.Add(new ItemComponentModel()
             {
                 Name = "User",
                 Route = new ItemComponentModel.RouteModel() { Controller = "User", Action = "Index" },
-                ImageUrl = "https://getbootstrap.com/docs/5.2/examples/features/unsplash-photo-1.jpg"
+                ImageUrl = "/pics/user.jpg"
             });
             return itemModelList;
         }
